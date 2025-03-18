3cca7a9e7a36a59c5159aeb879c1b253ac4d91b3
renz-lanuza <cjdezz07@gmail.com>
2025-03-18 15:24:31 +0800

    Update modal_for_unit.php

--- admin/modals/modal_for_unit.php
+++ admin/modals/modal_for_unit.php
@@ -23,3 +23,30 @@
         </div>
     </div>
 </div>
+
+<!-- Update Unit Modal -->
+<div class="modal fade" id="updateUnitModal" tabindex="-1" aria-labelledby="updateUnitModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #0D7C66; color: white;">
+                <h5 class="modal-title" id="updateUnitModalLabel">Update Unit</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="updateUnitForm">
+                    <input type="hidden" id="unit_id" name="unit_id">
+                    <div class="form-group">
+                        <label for="up_unit_name">Unit Name</label>
+                        <input type="text" class="form-control" id="up_unit_name" name="unit_name" required>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-success">Update</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
