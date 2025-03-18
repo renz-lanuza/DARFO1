9bc8526b6b8d81c410e8cd30a9dba76ce88942fb
renz-lanuza <cjdezz07@gmail.com>
2025-03-18 15:55:22 +0800

    Add files via upload

---
+++ admin/8beneficiaryManagement/fetch_beneficiaries.php
@@ -0,0 +1,53 @@
+<?php
+include('../../conn.php');
+
+$category = isset($_GET['category']) ? $_GET['category'] : 'all';
+
+$query = "SELECT beneficiary_id, fname, mname, lname, rsbsa_no, province_name, municipality_name, barangay_name, birthdate, beneficiary_category FROM tbl_beneficiary";
+
+if ($category !== 'all') {
+    $query .= " WHERE beneficiary_category = ?";
+}
+
+$stmt = $conn->prepare($query);
+
+if ($category !== 'all') {
+    $stmt->bind_param("s", $category);
+}
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+function formatRsbsaNo($rsbsa_no) {
+    if (strlen($rsbsa_no) === 15) {
+        return substr($rsbsa_no, 0, 2) . '-' .
+            substr($rsbsa_no, 2, 2) . '-' .
+            substr($rsbsa_no, 4, 2) . '-' .
+            substr($rsbsa_no, 6, 3) . '-' .
+            substr($rsbsa_no, 9, 6);
+    }
+    return $rsbsa_no;
+}
+
+$data = [];
+
+while ($row = $result->fetch_assoc()) {
+    $fullName = $row['fname'] . ' ' . (!empty($row['mname']) ? $row['mname'] . ' ' : '') . $row['lname'];
+    $formattedBirthdate = date('F j, Y', strtotime($row['birthdate']));
+    $rsbsa_no = !empty($row['rsbsa_no']) ? formatRsbsaNo($row['rsbsa_no']) : 'N/A';
+
+    $data[] = [
+        'beneficiary_id' => $row['beneficiary_id'],
+        'fullName' => $fullName,
+        'rsbsa_no' => $rsbsa_no,
+        'province_name' => $row['province_name'],
+        'municipality_name' => $row['municipality_name'],
+        'barangay_name' => $row['barangay_name'],
+        'birthdate' => $formattedBirthdate
+    ];
+}
+
+echo json_encode($data);
+$stmt->close();
+$conn->close();
+?>
