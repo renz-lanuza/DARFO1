584ee0423b1e5f40b8cc0ec5b7aa77c18d8197ee
renz-lanuza <cjdezz07@gmail.com>
2025-03-18 09:22:34 +0800

    Update fetch_intervention.php

--- admin/2InterventionManagement/fetch_intervention.php
+++ admin/2InterventionManagement/fetch_intervention.php
@@ -10,15 +10,13 @@ if (isset($_POST['intervention_id'])) {
 
     $query = "SELECT 
                 i.intervention_id, 
-                i.int_type_id, 
                 t.intervention_name, 
                 i.description, 
                 i.quantity, 
                 i.quantity_left,
-                i.seed_id,
-                i.unit_id,
                 s.seed_name,
-                u.unit_name
+                u.unit_id,  
+                u.unit_name  
               FROM tbl_intervention_inventory i
               JOIN tbl_intervention_type t ON i.int_type_id = t.int_type_id
               JOIN tbl_seed_type s ON i.seed_id = s.seed_id
@@ -31,7 +29,17 @@ if (isset($_POST['intervention_id'])) {
     $result = $stmt->get_result();
 
     if ($row = $result->fetch_assoc()) {
-        echo json_encode($row);
+        error_log("Fetched unit_id: " . $row["unit_id"]); // Debugging
+        echo json_encode([
+            "intervention_id" => $row["intervention_id"],
+            "intervention_name" => $row["intervention_name"],
+            "description" => $row["description"],
+            "quantity" => $row["quantity"],
+            "quantity_left" => $row["quantity_left"],
+            "seed_name" => $row["seed_name"],
+            "unit_id" => $row["unit_id"],  // Hidden field
+            "unit_name" => $row["unit_name"] // Displayed in the dropdown
+        ]);
     } else {
         echo json_encode(["error" => "No record found"]);
     }
