7ceed983a3e6a0445e1517c30111bb96d8663a21
onlyraps <163240674+onlyraps@users.noreply.github.com>
2025-03-27 08:06:42 +0800

    Update modal_for_distribution.php

--- admin/modals/modal_for_distribution.php
+++ admin/modals/modal_for_distribution.php
@@ -184,7 +184,6 @@
         background-color: #D1ECE4;
     }
 </style>
-
 <!-- Update Distribution Modal -->
 <div class="modal fade" id="updateDistributionModal" tabindex="-1" aria-labelledby="updateDistributionModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
@@ -268,30 +267,6 @@
     });
 </script>
 
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        var updateDistributionModal = document.getElementById('updateDistributionModal');
-        updateDistributionModal.addEventListener('show.bs.modal', function(event) {
-            var button = event.relatedTarget; // Button that triggered the modal
-            var distributionId = button.getAttribute('data-distribution-id');
-            var quantity = button.getAttribute('data-quantity');
-            var interventionName = button.getAttribute('data-intervention-name');
-            var seedName = button.getAttribute('data-seed-name');
-            var distributionDate = button.getAttribute('data-distribution-date');
-
-            // Update the modal's content.
-            var modalTitle = updateDistributionModal.querySelector('.modal-title');
-            var distributionIdInput = updateDistributionModal.querySelector('#distribution_id');
-            var updateQuantityInput = updateDistributionModal.querySelector('input[name="update_quantity[]"]');
-            var updateDistributionDateInput = updateDistributionModal.querySelector('#update_distribution_date');
-
-            modalTitle.textContent = 'Update Distribution ' + distributionId;
-            distributionIdInput.value = distributionId;
-            updateQuantityInput.value = quantity;
-            updateDistributionDateInput.value = distributionDate;
-        });
-    });
-</script>
 
 <!-- function for update distribution -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
